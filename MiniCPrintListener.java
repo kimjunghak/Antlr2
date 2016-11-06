dce99d2959b69d27bbe798aba4195aa786d44d05
kimjunghak <kboy1025@gmail.com>
2016-11-06 23:19:54 +0900

    antlr

--- MiniCPrintListener.java
+++ MiniCPrintListener.java
@@ -1,3 +1,4 @@
+import org.antlr.v4.runtime.ParserRuleContext;
 import org.antlr.v4.runtime.tree.ParseTreeProperty;
 
 /**
@@ -15,9 +16,96 @@ public class MiniCPrintListener extends MiniCBaseListener {
         return ctx.getChildCount() == 2 & ctx.getChild(0) != ctx.expr();
     }
 
+
+    @Override
+    public void exitProgram(MiniCParser.ProgramContext ctx) {
+
+    }
+
+    @Override
+    public void exitDecl(MiniCParser.DeclContext ctx) {
+
+    }
+
+    @Override
+    public void exitVar_decl(MiniCParser.Var_declContext ctx) {
+
+    }
+
+    @Override
+    public void exitType_spec(MiniCParser.Type_specContext ctx) {
+        String s1, s2;
+
+        s1 = ctx.getChild(0).getText();
+        s2 = ctx.getParent().getChild(1).getText();
+        newTexts.put(ctx, s1+ " " + s2);
+
+        System.out.println(newTexts.get(ctx));
+    }
+
+    @Override
+    public void exitFun_decl(MiniCParser.Fun_declContext ctx) {
+
+    }
+
+    @Override
+    public void exitParams(MiniCParser.ParamsContext ctx) {
+
+    }
+
+    @Override
+    public void exitParam(MiniCParser.ParamContext ctx) {
+
+    }
+
+    @Override
+    public void exitStmt(MiniCParser.StmtContext ctx) {
+
+    }
+
+    @Override
+    public void exitExpr_stmt(MiniCParser.Expr_stmtContext ctx) {
+
+    }
+
+    @Override
+    public void exitWhile_stmt(MiniCParser.While_stmtContext ctx) {
+
+    }
+
+    @Override
+    public void exitCompound_stmt(MiniCParser.Compound_stmtContext ctx) {
+
+    }
+
+    @Override
+    public void exitLocal_decl(MiniCParser.Local_declContext ctx) {
+
+    }
+
+    @Override
+    public void exitIf_stmt(MiniCParser.If_stmtContext ctx) {
+        String paren1 = null, paren2 = null, s1 = null, s2 = null;
+
+        paren1 = ctx.getChild(1).getText();
+        paren2 = ctx.getChild(3).getText();
+        s1 = ctx.getChild(2).getText();
+        s2 = newTexts.get(ctx.expr());
+
+        newTexts.put(ctx, paren1 + s1 + " " + s2 + paren2);
+
+        System.out.println(newTexts.get(ctx));
+
+    }
+
+    @Override
+    public void exitReturn_stmt(MiniCParser.Return_stmtContext ctx) {
+
+    }
+
     @Override
     public void exitExpr(MiniCParser.ExprContext ctx){
-        String s1 = null, s2 = null, op = null;
+        String s1, s2, op;
 
         if(isBinaryOperation(ctx)) {
 
@@ -37,43 +125,11 @@ public class MiniCPrintListener extends MiniCBaseListener {
         System.out.println(newTexts.get(ctx));
 
     }
-    @Override
-    public void exitType_spec(MiniCParser.Type_specContext ctx) {
-        String s1 = null, s2 = null;
-
-        s1 = ctx.getChild(0).getText();
-        newTexts.put(ctx, s1+ " ");
-
-        System.out.println(newTexts.get(ctx));
-    }
 
     @Override
-    public void exitIf_stmt(MiniCParser.If_stmtContext ctx) {
-        String paren1 = null, paren2 = null, s1 = null, s2 = null;
-
-        paren1 = ctx.getChild(1).getText();
-        paren2 = ctx.getChild(3).getText();
-        s1 = ctx.getChild(2).getText();
-        s2 = newTexts.get(ctx.expr());
-
-        newTexts.put(ctx, paren1 + s1 + " " + s2 + paren2);
-
-        System.out.println(newTexts.get(ctx));
+    public void exitArgs(MiniCParser.ArgsContext ctx) {
 
     }
 
-    @Override
-    public void exitWhile_stmt(MiniCParser.While_stmtContext ctx) {
-
-    }
-
-    @Override
-    public void exitCompound_stmt(MiniCParser.Compound_stmtContext ctx) {
-        String s1 = null, s2 = null, paren1 = null, paren2 = null;
-
-        paren1 = ctx.getChild(0).getText();
-        paren2 = ctx.getChild(2).getText();
-
-
-    }
 }
+
